063a17ed82469a5331ccf0497a58113a74d7b1af
jérémy vaneste <jeremy.vaneste@free.fr>
2022-04-26 16:31:17 +0200

    structuration footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./footer.css";
+import { GrReactjs } from "react-icons/gr";
+import { BsLinkedin, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="container_footer">
+      <div className="main_part_footer">
+        <GrReactjs className="footer_icon"></GrReactjs>
+        <a href="#">Accueil</a>
+        <a href="#about">A propos</a>
+        <a href="#competences">Compétences</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      <div className="second_part_footer">
+        <small>&copy; Jérémy Vaneste</small>
+        <div className="socials_icon_container">
+          <BsLinkedin className="social_icon"></BsLinkedin>
+          <BsGithub className="social_icon"></BsGithub>
+          <BsInstagram className="social_icon"></BsInstagram>
+          <BsTwitter className="social_icon"></BsTwitter>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

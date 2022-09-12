1551362b93960d142469a4bd9e4472f3f97d554e
jérémy vaneste <jeremy.vaneste@free.fr>
2022-09-12 17:21:32 +0200

    add links to linkedIn & github into footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./footer.css";
 import { GrReactjs } from "react-icons/gr";
 import { BsLinkedin, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -18,10 +19,19 @@ const Footer = () => {
       <div className="second_part_footer">
         <small>&copy; Jérémy Vaneste</small>
         <div className="socials_icon_container">
-          <BsLinkedin className="social_icon"></BsLinkedin>
-          <BsGithub className="social_icon"></BsGithub>
-          <BsInstagram className="social_icon"></BsInstagram>
-          <BsTwitter className="social_icon"></BsTwitter>
+          <a href="https://www.linkedin.com/in/j%C3%A9r%C3%A9my-vaneste-78a84b22b/">
+            <BsLinkedin className="social_icon"></BsLinkedin>
+          </a>
+
+          <a href="https://github.com/jeremyv59">
+            <BsGithub className="social_icon"></BsGithub>
+          </a>
+          <a>
+            <BsInstagram className="social_icon"></BsInstagram>
+          </a>
+          <a>
+            <BsTwitter className="social_icon"></BsTwitter>
+          </a>
         </div>
       </div>
     </footer>
